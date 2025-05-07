0deb76b51570c68db5148de30197af8bb79d1443
Simon Schrottner <simon.schrottner@dynatrace.com>
2025-05-07 18:30:51 +0200

    chore: add step 3.1
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>

--- src/main/java/dev/openfeature/demo/java/demo/IndexController.java
+++ src/main/java/dev/openfeature/demo/java/demo/IndexController.java
@@ -1,17 +1,21 @@
 package dev.openfeature.demo.java.demo;
 
-import dev.openfeature.sdk.Client;
-import dev.openfeature.sdk.FlagEvaluationDetails;
-import dev.openfeature.sdk.OpenFeatureAPI;
+import dev.openfeature.sdk.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 public class IndexController {
 
     @GetMapping("/")
-    public FlagEvaluationDetails<String> helloWorld() {
+    public FlagEvaluationDetails<String> helloWorld(@RequestParam(required = false) String language) {
         Client client = OpenFeatureAPI.getInstance().getClient();
-        return client.getStringDetails("greetings", "No World");
+        HashMap<String, Value> attributes = new HashMap<>();
+        attributes.put("language", new Value(language));
+        return client.getStringDetails("greetings", "Hello World",
+                new ImmutableContext(attributes));
     }
 }
