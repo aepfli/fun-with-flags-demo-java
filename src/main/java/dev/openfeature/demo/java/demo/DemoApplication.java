1ed86680b5b9ff6297f6bad40dc9c7526965ce7f
Simon Schrottner <simon.schrottner@dynatrace.com>
2025-05-07 20:56:42 +0200

    setup for demos with possible tracing
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>

--- src/main/java/dev/openfeature/demo/java/demo/DemoApplication.java
+++ src/main/java/dev/openfeature/demo/java/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package dev.openfeature.demo.java.demo;
 
+import dev.openfeature.contrib.hooks.otel.TracesHook;
+import dev.openfeature.sdk.OpenFeatureAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		OpenFeatureAPI.getInstance().addHooks(new TracesHook());
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
