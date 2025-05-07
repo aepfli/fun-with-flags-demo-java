1c5d6c7265afd1020357c8ff2a8f7d2a1fe53c9f
Simon Schrottner <simon.schrottner@dynatrace.com>
2025-05-07 18:18:48 +0200

    chore: add step 1.1
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>

--- src/main/java/dev/openfeature/demo/java/demo/IndexController.java
+++ src/main/java/dev/openfeature/demo/java/demo/IndexController.java
@@ -1,5 +1,8 @@
 package dev.openfeature.demo.java.demo;
 
+import dev.openfeature.sdk.Client;
+import dev.openfeature.sdk.FlagEvaluationDetails;
+import dev.openfeature.sdk.OpenFeatureAPI;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @GetMapping("/")
-    public String helloWorld() {
-        return "Hello World";
+    public FlagEvaluationDetails<String> helloWorld() {
+        Client client = OpenFeatureAPI.getInstance().getClient();
+        return client.getStringDetails("greetings", "No World");
     }
 }
